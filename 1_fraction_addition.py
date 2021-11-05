2751abfe0081e0f3bc7b427d2e3f9d7a691f477b
LynchXLQ <73268562+LynchXLQ@users.noreply.github.com>
2021-11-04 21:53:36 -0400

    Add files via upload

---
+++ 1_fraction_addition.py
@@ -0,0 +1,32 @@
+# Time: 2021/11/4  20:17
+class Fraction:
+    def __init__(self,top, bottom):
+        self.numerator = top
+        self.denominator = bottom
+
+    def show(self):
+        print(self.numerator, '/', self.denominator)
+
+    def gcd(self, a, b):   # greatest common divisor，GCD
+        while a%b != 0:
+            old_a = a
+            old_b = b
+            a = old_b
+            b = old_a%old_b
+        return b
+
+    def __str__(self):
+        return str(self.numerator) + '/' + str(self.denominator)
+
+    def __add__(self, other:'Fraction'):
+        new_numerator = self.numerator*other.denominator + self.denominator*other.numerator
+        new_denominator = self.denominator*other.denominator
+        common = self.gcd(new_numerator, new_denominator)
+        return Fraction(new_numerator//common, new_denominator//common)
+
+
+
+f1 = Fraction(1, 4)
+f2 = Fraction(1, 2)
+f3 = f1 + f2
+print(f3.__str__())
